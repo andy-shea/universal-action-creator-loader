9acf6c765eea854d6b9b10f71b78eddeb01c5048
Andy Shea <andy@tjs.com>
2023-09-18 22:28:10 +0100

    Dependency update

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
-var acorn = require('acorn');
-var walk = require('acorn/dist/walk');
-var astring = require('astring');
+var acorn = require("acorn");
+var walk = require("acorn-walk");
+var astring = require("astring");
 
 function isObject(nodeType, node) {
-  return (nodeType === 'ObjectExpression' && node.properties.length);
+  return nodeType === "ObjectExpression" && node.properties.length;
 }
 
-module.exports = function(source, map) {
+module.exports = function (source, map) {
   this.cacheable();
 
   var ast = acorn.parse(source, {
-    sourceType: 'module',
-    ecmaVersion: 8
+    sourceType: "module",
+    ecmaVersion: 8,
   });
 
   var found = walk.findNodeAfter(ast, 0, isObject);
   while (found) {
     var properties = found.node.properties;
-    var serverIndex = properties.findIndex(function(property) {
-      return property.key.name === 'server';
+    var serverIndex = properties.findIndex(function (property) {
+      return property.key.name === "server";
     });
     if (serverIndex !== -1) properties.splice(serverIndex, 1);
 
-    found = walk.findNodeAfter(ast, properties.length ? properties[0].start : found.node.end, isObject);
+    found = walk.findNodeAfter(
+      ast,
+      properties.length ? properties[0].start : found.node.end,
+      isObject
+    );
   }
 
-  this.callback(null, astring.generate(ast, {indent: '  '}), map);
+  this.callback(null, astring.generate(ast, { indent: "  " }), map);
 };
